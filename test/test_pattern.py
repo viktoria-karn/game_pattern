c037262f37e10e2856b3fc2059a61321ecdfe65a
viktoria-karn <karnauhova01@bk.ru>
2023-04-12 02:37:09 +0700

    Correct name game creation test

--- test/test_pattern.py
+++ test/test_pattern.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, main
 from game_pattern import Game_pattern
 
-class CalcTest(TestCase):
+class GameTest(TestCase):
     def test_construction(self):
         assert Game_pattern()
