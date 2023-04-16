cdbf9c0cb848726c3a04c705f81847837eb304c6
viktoria-karn <karnauhova01@bk.ru>
2023-04-16 17:09:28 +0700

    Creation test_singleton

--- test/test_pattern.py
+++ test/test_pattern.py
@@ -1,6 +1,13 @@
 from unittest import TestCase, main
 from game_pattern import Game_pattern
+#import game_pattern as gp
 
 class GameTest(TestCase):
     def test_construction(self):
         assert Game_pattern()
+
+    def test_singleton(self):
+        g = Game_pattern()
+        r_a,a = g.singleton_p()
+        self.assertEqual(r_a,"singleton")
+
