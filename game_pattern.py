8f093890cd2729988b3cd2987f7be57aa5051637
viktoria-karn <karnauhova01@bk.ru>
2023-04-16 19:30:20 +0700

    Create function singleton_p

--- game_pattern.py
+++ game_pattern.py
@@ -1,2 +1,8 @@
 class Game_pattern:
-    pass
+    def singleton_p(self):
+        print("Очередной порождающий паттерн. С помощью него обеспечивается наличие единственного экземпляра класса"
+              " с глобальной точкой доступа в однопоточном приложении.")
+        print("Введите правильный ответ")
+        right_answer = "singleton"
+        answer = input()
+        return right_answer, answer
